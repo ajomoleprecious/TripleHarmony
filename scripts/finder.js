6a3e79f27c1c4eb6ec35359f92cae481f42c3e2b
ajomole_precious <precious.ajomole@outlook.com>
2024-03-23 03:59:57 +0100

    added pokemon finder game

---
+++ scripts/finder.js
@@ -0,0 +1,207 @@
+const NUM_BUSHES_PC = 2000
+const NUM_BUSHES_MOBILE = 400
+const NUM_BALLS = 5
+
+const player = document.querySelector('.player')
+const player_pos = {
+    x: parseInt(window.innerWidth / 2),
+    y: parseInt(window.innerHeight / 2)
+}
+const player_vel = {
+    x: 0,
+    y: 0
+}
+const balls = []
+//const sound = new Audio('assets/coin.mp3')
+const mainTag = document.querySelector('main');
+
+function createBushes() {
+    const mainTag = document.querySelector('main');
+
+    if (window.innerWidth <= 800) { // Check for window width of 800 pixels or lower
+        document.body.style.backgroundSize = "cover";
+        for (let i = 0; i < NUM_BUSHES_MOBILE; i++) {
+            const div = document.createElement('div');
+            div.classList.add('bush');
+            div.style.left = Math.max(1, Math.random() * (window.innerWidth - 50)) + 'px'; // Random X position
+            div.style.top = Math.max(10, Math.random() * (window.innerHeight - 110)) + 'px'; // Random Y position
+            mainTag.appendChild(div);
+        }
+    } else {
+        for (let i = 0; i < NUM_BUSHES_PC; i++) {
+            const div = document.createElement('div');
+            div.classList.add('bush');
+            // Limiting the range of random positions for PC view
+            div.style.left = Math.max(1, Math.random() * (window.innerWidth - 55)) + 'px'; // Minimum 1px from left
+            div.style.top = Math.max(10, Math.random() * (window.innerHeight - 100)) + 'px'; // Minimum 1px from top
+            mainTag.appendChild(div);
+        }
+    }
+}
+
+
+
+function generateBall() {
+    // Check if there are fewer balls than the desired number before generating a new ball
+    if (balls.length < NUM_BALLS) {
+        const div = document.createElement('div');
+        div.classList.add('pokeball');
+        div.style.position = 'absolute';
+        
+        // Calculate the maximum X and Y positions
+        const maxX = window.innerWidth - 50; // Maximum X position
+        const maxY = window.innerHeight - 110; // Maximum Y position
+
+        // Calculate the minimum X and Y positions
+        const minX = Math.min(1, maxX); // Minimum X position (at least 25px from left)
+        const minY = Math.min(10, maxY); // Minimum Y position (at least 10px from top)
+
+        // Generate random positions within the bounds
+        let x = Math.max(minX, Math.random() * maxX) + 'px'; // Random X position
+        let y = Math.max(minY, Math.random() * maxY) + 'px'; // Random Y position
+
+        div.style.left = x;
+        div.style.top = y;
+        balls.push({
+            ball: div,
+            pos: {
+                x,
+                y
+            }
+        });
+        // Append the ball to the <main> tag
+        mainTag.appendChild(div);
+    }
+}
+
+
+
+
+function createBalls() {
+    for (let i = 0; i < NUM_BALLS; i++) {
+        generateBall()
+    }
+}
+
+function collision(ball, player) {
+    var ballRect = ball.getBoundingClientRect();
+    var playerRect = player.getBoundingClientRect();
+
+    var x1 = ballRect.left;
+    var y1 = ballRect.top;
+    var h1 = ballRect.height;
+    var w1 = ballRect.width;
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+
+    var x2 = playerRect.left;
+    var y2 = playerRect.top;
+    var h2 = playerRect.height;
+    var w2 = playerRect.width;
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
+
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
+        return false;
+    }
+    return true;
+}
+
+let modal;
+function checkCollisions() {
+    balls.forEach((ball, index) => {
+        if (collision(ball.ball, player)) {
+            //sound.play();
+            ball.ball.remove();
+            balls.splice(index, 1);
+            generateBall();
+            modal = new bootstrap.Modal(document.getElementById('pokemonFinder'), {
+                keyboard: false
+            });
+            modal.show();
+        }
+    });
+}
+
+
+function run() {
+    player_pos.x += player_vel.x;
+    player_pos.y += player_vel.y;
+
+    // Check if the player exceeds the screen bounds on X-axis
+    if (player_pos.x < 0) {
+        player_pos.x = 0;
+    } else if (player_pos.x > window.innerWidth - player.offsetWidth) {
+        player_pos.x = window.innerWidth - player.offsetWidth;
+    }
+    // Check if the player exceeds the screen bounds on Y-axis
+    if (player_pos.y < 0) {
+        player_pos.y = 0;
+    } else if (player_pos.y > window.innerHeight - player.offsetHeight) {
+        player_pos.y = window.innerHeight - player.offsetHeight;
+    }
+
+    player.style.left = player_pos.x + 'px';
+    player.style.bottom = player_pos.y + 'px';
+
+    checkCollisions();
+
+    requestAnimationFrame(run);
+}
+
+function init() {
+    createBushes()
+    createBalls()
+    run()
+}
+
+init()
+
+const keysPressed = new Set();
+
+window.addEventListener('keydown', function (e) {
+    keysPressed.add(e.key);
+    
+    // Check if modal is shown
+    if (modal && modal._element.classList.contains('show')) {
+        // If modal is shown, do not update player velocity
+        player_vel.x = 0;
+        player_vel.y = 0;
+    } else {
+        // Update player velocity based on pressed keys
+        if (keysPressed.has("ArrowUp")) {
+            player_vel.y = .35;
+            player.style.backgroundImage = 'url("../assets/finder-files/player_front.png")';
+        }
+        if (keysPressed.has("ArrowDown")) {
+            player_vel.y = -.35;
+            player.style.backgroundImage = 'url("../assets/finder-files/player_back.png")';
+        }
+        if (keysPressed.has("ArrowLeft")) {
+            player_vel.x = -.35;
+            player.style.backgroundImage = 'url("../assets/finder-files/player_left.png")';
+        }
+        if (keysPressed.has("ArrowRight")) {
+            player_vel.x = .35;
+            player.style.backgroundImage = 'url("../assets/finder-files/player_right.png")';
+        }
+        player.classList.add('active');
+    }
+});
+
+window.addEventListener('keyup', function (e) {
+    keysPressed.delete(e.key);
+
+    // Update player velocity based on remaining pressed keys
+    if (!keysPressed.has("ArrowUp") && !keysPressed.has("ArrowDown")) {
+        player_vel.y = 0;
+    }
+    if (!keysPressed.has("ArrowLeft") && !keysPressed.has("ArrowRight")) {
+        player_vel.x = 0;
+    }
+
+    // Remove active class only if no keys are pressed
+    if (keysPressed.size === 0) {
+        player.classList.remove('active');
+    }
+});
---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+let body = document.querySelector("body");
+let projects = document.querySelectorAll(".projects ul li");
+let projectsContainer = document.querySelector(".projects");
+function showBg() {
+    let body = document.querySelector("body");
+    let projects = document.querySelectorAll(".projects ul li");
+    let projectsContainer = document.querySelector(".projects");
+
+    projects.forEach((project, index) => {
+        project.addEventListener("mouseover", () => {
+            body.style.backgroundImage = `url('assets/landing-page-bgs/bg_${getImageName(index)}.jpg')`;
+            body.style.backgroundSize = "cover";
+            body.style.backgroundPosition = "center";
+            body.style.backgroundRepeat = "no-repeat";
+            body.style.width = "100%";
+            body.style.height = "100vh";
+        });
+
+        project.addEventListener("mouseout", () => {
+            body.style.backgroundImage = "none";
+        });
+    });
+
+    function getImageName(index) {
+        let projectNames = ["pokemon", "fifa", "fortnite", "lotr", "lego", "magic"];
+        return projectNames[index];
+    }
+}
+function checkScreenSize() {
+    if (window.innerWidth >= 1400) {
+        showBg();
+    }
+}
+window.onload = checkScreenSize;
+window.addEventListener("resize", checkScreenSize);
