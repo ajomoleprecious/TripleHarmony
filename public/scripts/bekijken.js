5b248ab41b772f06e4e0084b97548b325ad44094
ajomole_precious <precious.ajomole@outlook.com>
2024-04-03 19:14:30 +0000

    almost done with showing pokemons on paging

--- public/scripts/bekijken.js
+++ public/scripts/bekijken.js
@@ -1,36 +1,42 @@
 let amountOfpokemons = document.getElementById('amountOfPokemons');
 let listForm = document.getElementById('getAmountOfPokemons');
 let body = document.querySelector('body');
-/*
 // on DOMContentloaded
-document.addEventListener('DOMContentLoaded', function() {
-    // get the value from local storage
-    if (localStorage.getItem('amountOfPokemons')) {
-        amountOfpokemons.value = localStorage.getItem('amountOfPokemons');
-    }
-    else {
-        amountOfpokemons.value = 50;
-    }
-    // on change
-    amountOfpokemons.addEventListener('change', function() {
-        // store the value in local storage
-        localStorage.setItem('amountOfPokemons', amountOfpokemons.value);
-        listForm.submit();
-    });
 
-    
+// get the value from local storage
+if (localStorage.getItem('amountOfPokemons')) {
+    amountOfpokemons.value = localStorage.getItem('amountOfPokemons');
+}
+else {
+    amountOfpokemons.value = 50;
+}
+// on change
+amountOfpokemons.addEventListener('change', function () {
+    // store the value in local storage
+    localStorage.setItem('amountOfPokemons', amountOfpokemons.value);
+    listForm.submit();
 });
-*/  // CODE TO CHANGE THE AMOUNT OF POKEMONS TO SHOW ON THE PAGE LOAD (NOT WORKING YET)
+
 
 let previousButton = document.getElementById('previous');
 let nextButton = document.getElementById('next');
 
-previousButton.addEventListener('click', function() {
-    listForm.submit();
-    
+previousButton.addEventListener('click', function () {
+    let currentPage = parseInt(listForm.querySelector('input[name="page"]').value);
+    if (currentPage > 1) {
+        listForm.querySelector('input[name="page"]').value = currentPage - 1;
+        localStorage.setItem('page', currentPage - 1);
+        listForm.submit();
+    }
 });
-nextButton.addEventListener('click', function() {
-    
+
+nextButton.addEventListener('click', function () {
+    let currentPage = parseInt(listForm.querySelector('input[name="page"]').value);
+    listForm.querySelector('input[name="page"]').value = currentPage + 1;
+    localStorage.setItem('page', currentPage + 1);
     listForm.submit();
-    
 });
+
+if (localStorage.getItem('page')) {
+    listForm.querySelector('input[name="page"]').value = localStorage.getItem('page');
+}
