eda48d30ac329053cdf516d3e62de6e721726452
aziz <azizkarimov979@gmail.com>
2024-04-24 15:35:12 +0000

    eerste stap

---
+++ public/scripts/submenu.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    let modal = new bootstrap.Modal(document.getElementById('receive-poke'), {
+        keyboard: false,
+        backdrop: false
+    });
+    modal.show();
+}
