740e73363134f302b2935ac07c56cebe2f053dfb
Precious <precious.ajomole@outlook.com>
2024-04-24 20:20:40 +0000

    added routers for most pages

--- middleware/verifyUser.ts
+++ middleware/verifyUser.ts
@@ -2,24 +2,31 @@ import { Request, Response, NextFunction } from "express";
 const User = require('../models/User') as any;
 const jwt = require('jsonwebtoken');
 
+const maxAge = 1 * 24 * 60 * 60;
+
 export const verifyUser = (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies.jwt;
     if (token) {
         jwt.verify(token, 'Precious_Aziz_Mohammed', async (err : any, decodedToken : any) => {
             if (err) {
-                //console.log(err.message);
                 res.locals.user = null;
                 res.redirect('/pokemon-auth');
             } else {
-                //console.log(decodedToken);
-                //let user = await User.findById(decodedToken.id);
-                //res.locals.user = user;
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                // Check if token is about to expire within the next 30 minutes
+                const now = Math.floor(Date.now() / 1000);
+                if (decodedToken.exp - now < 1800) {
+                    // Refresh token
+                    const newToken = jwt.sign({ id: decodedToken.id }, 'Precious_Aziz_Mohammed', { expiresIn: maxAge });
+                    res.cookie('jwt', newToken, { httpOnly: true, maxAge: maxAge * 1000 });
+                }
                 next();
             }
         });
     }
     else {
-        //res.locals.user = null;
+        res.locals.user = null;
         res.redirect('/pokemon-auth');
     }
 }
