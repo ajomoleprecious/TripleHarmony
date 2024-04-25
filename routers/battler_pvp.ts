7938159bdf92d08dfe5aa382eef8b29876793637
Precious <precious.ajomole@outlook.com>
2024-04-25 23:32:29 +0000

    added connection

--- routers/battler_pvp.ts
+++ routers/battler_pvp.ts
@@ -10,11 +10,20 @@ router.use(verifyUser);
 
 const server = http.createServer(app);
 const io = new Server(server);
+const socket = io.on("connection", (socket: any) => {
+    socket.on("join-room", (roomID: string) => {
+        socket.join(roomID);
+        socket.to(roomID).emit("user-connected", socket.id);
+    });
+});
 
 router.get("/", (req: Request, res: Response) => {
     // get full URL
-    const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    //const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    const url = 'https' + '://' + req.get('host') + req.originalUrl;
     const roomID = req.query.roomID ? `${url}/?roomID=${req.params.roomID}` : `${url}/?roomID=${Math.floor(Math.random() * 1000)}`;
+
+    socket.emit("connection", roomID);
     res.render("pokemon-battler", { roomID });
 });
 
