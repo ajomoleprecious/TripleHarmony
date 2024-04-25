4d6d0676a68b3c3ad11e83cb53121994f8a99ab2
Precious <preciouscoolcool@gmail.com>
2024-04-25 11:01:42 +0000

    started work

--- routers/battler_pvp.ts
+++ routers/battler_pvp.ts
@@ -1,8 +1,15 @@
 import { Request, Response, Router } from "express";
 import { verifyUser } from "../middleware/verifyUser";
+const http = require("http");
+const { Server } = require("socket.io");
+const app = require("express");
+
 
 const router = Router();
 router.use(verifyUser);
+
+const server = http.createServer(app);
+const io = new Server(server);
 
 router.get("/", (req: Request, res: Response) => {
     res.render("pokemon-battler");
