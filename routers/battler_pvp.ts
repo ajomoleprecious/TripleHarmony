00138bc2bfd9297d5e46c157db44e675ea05b71c
Precious <precious.ajomole@outlook.com>
2024-04-27 23:56:44 +0000

    added avatar system for users

--- routers/battler_pvp.ts
+++ routers/battler_pvp.ts
@@ -1,9 +1,12 @@
 import { Request, Response, Router } from "express";
 import { verifyUser } from "../middleware/verifyUser";
+import { currentAvatar } from "../middleware/userAvatar";
+import { client } from "../index";
 import express from "express";
 
 const router = Router();
 router.use(verifyUser);
+router.use(currentAvatar);
 // Serve static files from the 'public' directory
 router.use(express.static('public'));
 
@@ -17,16 +20,27 @@ const io = require("socket.io")(server, {
 
 // Socket.io
 io.on("connection", (socket: any) => {
-    console.log("User connected: " + socket.id);
+  console.log("User connected: " + socket.id);
 });
 
 // http://localhost:3000/pokemon-battler
 router.get("/", (req: Request, res: Response) => {
-    // get full URL
-    const url = 'http' + '://' + req.get('host') + req.originalUrl;
-    const roomID = req.query.roomID? `${url}/?roomID=${req.query.roomID}` : `${url}/?roomID=${Math.floor(Math.random() * 1000)}`;
+  // get full URL
+  const url = 'http' + '://' + req.get('host') + req.originalUrl;
+  const roomID = req.query.roomID ? `${url}/?roomID=${req.query.roomID}` : `${url}/?roomID=${Math.floor(Math.random() * 1000)}`;
+  const avatar = res.locals.currentAvatar;
+  res.render("pokemon-battler", { roomID, avatar });
+});
 
-    res.render("pokemon-battler", { roomID });
+router.post("/change-avatar/:avatar", async (req, res) => {
+  res.locals.avatar = req.params.avatar;
+  try {
+      await client.db("users").collection("usersAvatars").updateOne({ _id: res.locals.user._id }, { $set: { avatar: res.locals.avatar } }, { upsert: true });
+      res.status(200).json({ message: "Avatar is succesvol gewijzigd." });
+  }
+  catch (err) {
+      console.error(err);
+  }
 });
 
 export default router;
