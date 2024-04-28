b9398ca1525e47d901a2caf5dfeb72875890c741
aziz <azizkarimov979@gmail.com>
2024-04-28 17:04:52 +0000

    fetch functie toegevoegd

--- routers/vergelijken.ts
+++ routers/vergelijken.ts
@@ -12,7 +12,10 @@ router.use(currentAvatar);
 // Serve static files from the 'public' directory
 router.use(express.static('public'));
 
+let pokemonArray: any[] = [];
+
 router.get("/", async (req: Request, res: Response) => {
+    fetchPokemons();
     const avatar = res.locals.currentAvatar;
     const currentPokemon = res.locals.currentPokemon;
     const user = await client.db('users').collection('usersPokemons').findOne({ _id: res.locals.user._id });
@@ -32,5 +35,28 @@ router.post("/change-avatar/:avatar", async (req, res) => {
     }
 });
 
+async function fetchPokemons()
+{
+    try 
+    {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1000');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        const pokemons = data.results;
+        
+        // Loop door de lijst met Pokémon en voeg ze toe aan de array
+        pokemons.forEach(async (pokemon: any) => {
+            const pokemonData = await fetch(pokemon.url);
+            const pokemonDetails = await pokemonData.json();
+            pokemonArray.push(pokemonDetails);
+        });
+    }
+    catch (error) 
+    {
+        console.error('Er is een fout opgetreden bij het ophalen van de Pokémon:', error);
+    }
+}
 
 export default router;
