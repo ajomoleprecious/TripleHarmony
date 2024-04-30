cebfe65c3d38375e3e7e927cfab86b699ee85176
Mohammed El Yabraki <elyab15@hotmail.com>
2024-04-30 08:06:37 +0000

    whosthat part 4

--- routers/who's.ts
+++ routers/who's.ts
@@ -2,12 +2,12 @@ import { Request, Response, Router } from "express";
 import { verifyUser } from "../middleware/verifyUser";
 import axios from "axios";
 import express from "express";
-
+ 
 const router = Router();
 router.use(verifyUser);
 // Serve static files from the 'public' directory
 router.use(express.static('public'));
-
+ 
 // Function to fetch a random Pokémon
 async function getRandomPokemon() {
     const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=1000");
@@ -16,16 +16,16 @@ async function getRandomPokemon() {
     const randomPokemon = pokemonList[randomIndex];
     return randomPokemon;
 }
-
+ 
 router.get('/', async (req: Request, res: Response) => {
     let randomPokemon = await getRandomPokemon();
     res.render('who\'s-that-pokemon', { name: randomPokemon.name, image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomPokemon.url.split("/")[6]}.png` });
 });
-
-
+ 
 router.get('/new-pokemon', async (req: Request, res: Response) => {
     let randomPokemon = await getRandomPokemon();
     res.json({ name: randomPokemon.name, image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomPokemon.url.split("/")[6]}.png` });
 });
-
+ 
+ 
 export default router;
