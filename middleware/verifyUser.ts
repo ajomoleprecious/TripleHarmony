a792534a958f9f9a674e37283ed59eca92c313ac
Precious <preciouscoolcool@gmail.com>
2024-05-22 11:44:37 +0000

    update on cookie :  it resaves on page load

--- middleware/verifyUser.ts
+++ middleware/verifyUser.ts
@@ -2,30 +2,32 @@ import { Request, Response, NextFunction } from "express";
 import { User } from "../models/User";
 const jwt = require('jsonwebtoken');
 
-const maxAge = 1 * 24 * 60 * 60;
+const maxAge = 1 * 24 * 60 * 60; // 1 day in seconds
 
 export const verifyUser = (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies.jwt;
     if (token) {
-        jwt.verify(token, process.env.SECRET_KEY , async (err : any, decodedToken : any) => {
+        jwt.verify(token, process.env.SECRET_KEY, async (err: any, decodedToken: any) => {
             if (err) {
                 res.locals.user = null;
                 res.redirect('/pokemon-auth');
             } else {
                 let user = await User.findById(decodedToken.id);
-                res.locals.user = user;
-                // Check if token is about to expire within the next 30 minutes
-                const now = Math.floor(Date.now() / 1000);
-                if (decodedToken.exp - now < 1800) {
-                    // Refresh token
-                    const newToken = jwt.sign({ id: decodedToken.id }, process.env.SECRET_KEY , { expiresIn: maxAge });
+                if (user) {
+                    res.locals.user = user;
+
+                    // Create a new token
+                    const newToken = jwt.sign({ id: decodedToken.id }, process.env.SECRET_KEY, { expiresIn: maxAge });
                     res.cookie('jwt', newToken, { httpOnly: true, maxAge: maxAge * 1000, secure: true });
+
+                    next();
+                } else {
+                    res.locals.user = null;
+                    res.redirect('/pokemon-auth');
                 }
-                next();
             }
         });
-    }
-    else {
+    } else {
         res.locals.user = null;
         res.redirect('/pokemon-auth');
     }
