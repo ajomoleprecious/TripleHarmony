ae4ce3eca424ba5061dc31e3007ddfb6a8014b5f
Precious <preciouscoolcool@gmail.com>
2024-05-30 10:40:38 +0000

    minified most css and js

--- public/scripts/battler-pc.js
+++ public/scripts/battler-pc.js
@@ -1,17 +1,1 @@
-let pokemonTofight = document.querySelector(".battlefield article:nth-child(2) img");
-let fightButtons = document.querySelectorAll(".aanvallen li");
-let punchAnim = document.querySelector(".pokemon-damage img:nth-child(2)");
-
-fightButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        punchAnim.style.display = "block";
-        pokemonTofight.style.animation = "damage .5s";
-        pokemonTofight.style.animationdelay = "1s";
-        setTimeout(() => {
-            punchAnim.style.display = "none";
-        }, 1000);
-        setTimeout(() => {
-            pokemonTofight.style.animation = "none";
-        }, 1500);
-    });
-});
+let pokemonTofight=document.querySelector(".battlefield article:nth-child(2) img"),fightButtons=document.querySelectorAll(".aanvallen li"),punchAnim=document.querySelector(".pokemon-damage img:nth-child(2)");fightButtons.forEach(e=>{e.addEventListener("click",()=>{punchAnim.style.display="block",pokemonTofight.style.animation="damage .5s",pokemonTofight.style.animationdelay="1s",setTimeout(()=>{punchAnim.style.display="none"},1e3),setTimeout(()=>{pokemonTofight.style.animation="none"},1500)})});
